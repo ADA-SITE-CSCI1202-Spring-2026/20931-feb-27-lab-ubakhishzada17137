a5532e2578095d3bec369e336808f7c6e4c52bde
JustUzik <ubakhishzade@icloud.com>
2026-02-27 09:50:13 +0400

    8.b

--- Person.java
+++ Person.java
@@ -46,3 +46,32 @@ public class Person {
                this.gender.equals(p.gender);
     }
 }
+class Teacher extends Person {
+    private String department;
+    private String[] courses;
+
+    public Teacher(String firstName, String lastName, String gender, String department, String[] courses) {
+        super(firstName, lastName, gender);
+        this.department = department;
+        this.courses = courses;
+    }
+
+    public String getDepartment() { return department; }
+    public void setDepartment(String department) { this.department = department; }
+
+    public String[] getCourses() { return courses; }
+    public void setCourses(String[] courses) { this.courses = courses; }
+
+    @Override
+    public String toString() {
+        return super.toString() + " | Dept: " + department + " | Courses: " + java.util.Arrays.toString(courses);
+    }
+
+    public boolean equals(Teacher t) {
+        if (t == null) return false;
+        if (!super.equals(t)) return false;
+        
+        return this.department.equals(t.getDepartment()) && 
+               java.util.Arrays.equals(this.courses, t.getCourses());
+    }
+}
